dbf02901811369fca37e3783293a943f07868dae
Prajwal Nilopant <prajwalnilopant@gmail.com>
2025-08-22 18:13:53 -0400

    Client-Side-Routing-Setup

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const q = () => {
+  return (
+    <div>
+      <footer className="fixed bottom-0 left-0 w-full footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+        <aside>
+          <p>Copyright © {new Date().getFullYear()} - All right reserved by DevTinder Ltd</p>
+        </aside>
+      </footer>
+    </div>
+  );
+};
+
+export default q;
---
+++ src/components/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div className="text-4xl text-white">Login</div>;
+};
+
+export default Login;
