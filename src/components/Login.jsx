7d58af4edf626a71f302f3485da07efbd7772be0
Prajwal Nilopant <prajwalnilopant@gmail.com>
2025-08-23 09:56:22 -0400

    Created login page and linked it backend using Axios, Updated Navbar, Configured redux store,

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
-  return <div className="text-4xl text-white">Login</div>;
+  const [emailId, setEmailId] = useState("prajwal@gmail.com");
+  const [password, setPassword] = useState("Prajwal@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    // API call
+    try {
+      const res = await axios.post(BASE_URL + "/login", { emailId, password }, { withCredentials: true });
+      dispatch(addUser(res.data));
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <div className="card card-border bg-base-300 w-96 card-lg justify-center mx-auto my-10">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email</legend>
+            <input type="text" value={emailId} onChange={(e) => setEmailId(e.target.value)} className="input" placeholder="Enter your email" />
+            <legend className="fieldset-legend">Password</legend>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Enter your Password" />
+          </fieldset>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/utils/userSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: null,
+  reducers: {
+    addUser: (state, action) => {
+      return action.payload;
+    },
+    removeUser: (state, action) => {
+      return null;
+    },
+  },
+});
+
+export const { addUser, removeUser } = userSlice.actions; // Action creators are generated for each case reducer function
+export default userSlice.reducer; // Exporting the reducer to be used in the store
