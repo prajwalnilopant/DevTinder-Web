3382571b670725b7f1da82776f7d4f18e6e8e818
Prajwal Nilopant <prajwalnilopant@gmail.com>
2025-08-26 09:32:07 -0400

    Built userCard for Feed page

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,21 @@
+const UserCard = ({ user }) => {
+  const { firstName, lastName, photoURL, age, gender, about } = user;
+  return (
+    <div className="card bg-base-300 w-96 shadow-sm">
+      <figure>
+        <img src={user.photoURL} alt="photo" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        {age && gender && <p>{age + ", " + gender}</p>}
+        <p>{about}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Ignore</button>
+          <button className="btn btn-secondary">Interested</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
