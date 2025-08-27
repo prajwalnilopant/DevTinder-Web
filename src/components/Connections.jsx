71b410db7a3a7f48efcbed43e665a916aa9c27f7
Prajwal Nilopant <prajwalnilopant@gmail.com>
2025-08-27 13:46:55 -0400

    Built connections page

---
+++ src/components/Connections.jsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import { addConnections } from "../utils/connectionSlice";
+import { useEffect } from "react";
+
+const Connections = () => {
+  const connections = useSelector((store) => store.connections);
+  const dispatch = useDispatch();
+  const fetchConnections = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/connections", { withCredentials: true });
+      dispatch(addConnections(res?.data?.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchConnections();
+  }, []);
+
+  if (!connections) return;
+  if (connections.length === 0) return <h1>No connections found!</h1>;
+
+  return (
+    <div className="text-center my-3">
+      <h1 className="text-bold text-white text-3xl">Connections</h1>
+      {connections.map((connection) => {
+        const { _id, firstName, lastName, photoURL, age, gender, about } = connection;
+        return (
+          <div key={_id} className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
+            <div>
+              <img alt="photo" className="w-20 h-20 rounded-full" src={photoURL} />
+            </div>
+            <div className="text-left mx-4">
+              <h2 className="font-bold text-xl">{firstName + " " + lastName}</h2>
+              {age && gender && <p>{age + ", " + gender}</p>}
+              <p>{about}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Connections;
