2bebf350bf7e18cce83b6f2200505b81d6cfe132
Prajwal Nilopant <prajwalnilopant@gmail.com>
2025-09-03 22:52:31 -0400

    Updating the BASE_URL to make it dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,7 @@
-export const BASE_URL = "/api";
+// // For development
+// export const BASE_URL = "http://localhost:7777";
+
+// // For production
+// export const BASE_URL = "/api";
+
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7777" : "/api";
